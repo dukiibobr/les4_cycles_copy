93e7999c2fa86ffb71718e10b64f29eba8710632
chotkii bobr <danilo.kozubovskyi@gmail.com>
2026-09-04 19:50:17 +0300

    Add project files.

---
+++ les4_cycles_copy2/les4_cycles_copy2.cpp
@@ -0,0 +1,439 @@
+
+
+#include <iostream>
+using namespace std;
+
+void main()
+{
+
+
+
+	/*enum countries {
+	USA = 1,
+	France = 33,
+	Ukraine = 380,
+	Italy = 39,
+	Australia = 61
+	};
+
+	cout << USA << endl;
+
+
+	enum coins {
+		penny = 1,
+		nickel = 5,
+		dime = 10,
+		quarter = 25,
+		half = 50,
+		dollar_coin = 100
+	};
+
+
+	int coin;
+	cout << "enter value of coin: " << endl;
+	cin >> coin;
+
+	switch (coin)
+	{
+	case penny:cout << "penny is 1 cent" << endl;break;
+	case nickel:cout << "nickel is 5 cent" << endl;break;
+	case dime:cout << "dime is 10 cent" << endl;break;
+	case quarter:cout << "quarter is 25 cent" << endl;break;
+	case half:cout << "half is 50 cent" << endl;break;
+	case dollar_coin:cout << "dollar coin is 100 cent" << endl;break;
+	default:cout << "there are no such coin" << endl; break;
+	}*/
+
+
+
+
+
+	/* while (true) {
+
+	 }
+	 do while (true) {
+
+	 }
+	 for (size_t i = 0; i < length; i++)
+	 {
+
+	 }*/
+
+
+	 /*int counter = 20;
+	 while (counter < 15)
+	 {
+		 ++counter;
+		 cout << "i squatted " << counter << " times" << endl;
+	 }
+
+
+	 int seats = 20;
+	 do
+	 {
+		 ++seats;
+		 cout << "i squatted " << seats << " times" << endl;
+	 } while (seats < 15);
+
+
+	 float a, b, res;
+
+	 char key;
+
+	 cout << "Enter a : "; cin >> a;
+
+	 cout << "Enter b : "; cin >> b;
+
+	 cout << "\tChoose the operation : " << endl;;
+
+	 cout << " [+] - add number" << endl;
+
+	 cout << " [-] - sub number" << endl;
+
+	 cout << " [*] - multy number" << endl;
+
+	 cout << " [/] - div number" << endl;
+
+	 cout << " [e] - exit" << endl;
+
+	 cin >> key;
+
+	 while (key != 'e') {
+
+
+		 switch (key)
+
+		 {
+
+		 default:
+
+			 cout << "Error choice!!!!" << endl;
+
+			 break;
+
+		 case '+':
+
+			 res = a + b;
+
+			 cout << "Resualt = " << res << endl;
+
+			 break;
+
+		 case '-':
+
+			 res = a - b;
+
+			 cout << "Resualt = " << res << endl;
+
+			 break;
+
+		 case '*':
+
+			 res = a * b;
+
+			 cout << "Resualt = " << res << endl;
+
+			 break;
+
+		 case '/':
+
+
+
+			 cout << "end " << endl;
+
+			 break;
+
+		 case 'e':
+
+			 res = a / b;
+
+			 cout << "Resualt = " << res << endl;
+
+			 break;
+		 }
+		 cout << "Enter a : "; cin >> a;
+
+		 cout << "Enter b : "; cin >> b;
+
+		 cout << "\tChoose the operation : " << endl;;
+
+		 cout << " [+] - add number" << endl;
+
+		 cout << " [-] - sub number" << endl;
+
+		 cout << " [*] - multy number" << endl;
+
+		 cout << " [/] - div number" << endl;
+
+		 cout << " [e] - exit" << endl;
+
+		 cin >> key;*/
+
+		 //}
+
+
+
+
+	 //float a, b, res;
+	 //
+	 //char key;
+	 //do
+	 //{
+	 //	
+	 //	cout << "Enter a : "; cin >> a;
+	 //
+	 //	cout << "Enter b : "; cin >> b;
+	 //
+	 //	cout << "\tChoose the operation : " << endl;;
+	 //
+	 //	cout << " [+] - add number" << endl;
+	 //
+	 //	cout << " [-] - sub number" << endl;
+	 //
+	 //	cout << " [*] - multy number" << endl;
+	 //
+	 //	cout << " [/] - div number" << endl;
+	 //
+	 //	cout << " [e] - exit" << endl;
+	 //
+	 //	cin >> key;
+	 //
+	 //
+	 //	switch (key)
+	 //
+	 //	{
+	 //
+	 //	default:
+	 //
+	 //		cout << "Error choice!!!!" << endl;
+	 //
+	 //		break;
+	 //
+	 //	case '+':
+	 //
+	 //		res = a + b;
+	 //
+	 //		cout << "Resualt = " << res << endl;
+	 //
+	 //		break;
+	 //
+	 //	case '-':
+	 //
+	 //		res = a - b;
+	 //
+	 //		cout << "Resualt = " << res << endl;
+	 //
+	 //		break;
+	 //
+	 //	case '*':
+	 //
+	 //		res = a * b;
+	 //
+	 //		cout << "Resualt = " << res << endl;
+	 //
+	 //		break;
+	 //
+	 //	case '/':
+	 //
+	 //
+	 //
+	 //		cout << "end " << endl;
+	 //
+	 //		break;
+	 //
+	 //	case 'e':
+	 //		cout << "end" << endl;
+	 //		break;
+	 //	}
+	 //} while (key!='e');
+
+
+
+
+
+
+
+
+
+	 //for (int i = 0; i < 10; ++i)
+	 //{
+	 //	cout << i <<" ";
+	 //}
+	 //cout << endl;
+	 //
+	 //for (int i = 1; i <= 10; ++i)
+	 //{
+	 //	cout << i <<" ";
+	 //}
+	 //cout << endl;
+	 //
+	 //int i;
+	 //for ( i = 1; i <=10; i+=2)
+	 //{
+	 //	cout << i << " ";
+	 //}
+	 //cout << endl;
+	 //
+	 //
+	 //
+	 //
+	 //float x = 1;
+	 //
+	 //for (; x <=2; x+=0.2)
+	 //{
+	 //	cout << x << " ";
+	 //}
+	 //cout << endl;
+	 //
+	 //
+	 //for (int i=0;  i<5;)
+	 //{
+	 //	cout << i << " ";
+	 //	i++;
+	 //}
+	 //cout << endl;
+	 //
+	 //
+	 //for (int i=0; ; i++)
+	 //{
+	 //	cout << i << " ";
+	 //	if (i == 5) {
+	 //		break;
+	 //	}
+	 //}
+	 //cout << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+	 //int magic = 2;
+	 //cout << "=====================" << endl;
+	 //cout << "\t\tmy magic number between 1-10" << endl;
+	 //cout << "=====================" << endl;
+	 //
+	 //int user = 0;
+	 //
+	 //for (int i = 0; i <=5; i++)
+	 //{
+	 //	cout << "your number?" << endl; 
+	 //	cin >> user;
+	 //	if (user == magic) {
+	 //		cout << "you won" << endl;
+	 //		break;
+	 //	}
+	 //	if (i == 5) {
+	 //		cout << "you lost" << endl;
+	 //	}
+	 //	else {
+	 //		cout << "not my number" << endl;
+	 //	}
+
+	 //}
+
+
+
+
+
+
+	 //1
+
+	 //int count = 0;
+	 //while (count!=100)
+	 //{
+	 //	++count;
+	 //	cout << count << endl;
+	 //}
+
+
+	 //2 i %2=0 parni  counter ++
+
+	 //int count = 0;
+	 //int count2 = 0;
+	 //while (count!=200)
+	 //{
+	 //	count+=2;
+	 //	cout << count << endl;
+	 //	++count2;
+	 //}
+	 //cout << "there are" << count2 << "numbers" << endl;
+
+
+	 //3cin in cycle
+
+
+	 //int n;
+	 //cout << "enter number>0" << endl;
+	 //cin>>n;
+	 //
+	 //int count = 0;
+	 //int suma = 0;
+	 //int number;
+	 //
+	 //while (count<n)
+	 //{
+	 //	cout << "enter number" << endl;
+	 //	cin >> number;
+	 //	if (number % 2 == 0) {
+	 //		suma += number;
+	 //	}
+	 //	count++;
+	 //
+	 //}
+	 //cout << "suma: " << suma << endl;
+
+	 //int count;
+	 //cout << "enter count numbers" << endl;
+	 //cin >> count;
+	 //
+	 //
+	 //int i = 0;
+	 //int number;
+	 //int suma=0;
+	 //while (i<count)
+	 //{
+	 //	++i;
+	 //	cout << "enter" << i << "number" << endl;	
+	 //	cin >> number;
+	 //	if (number % 2 == 0) {
+	 //		suma += number;
+	 //	}
+	 //	
+	 //}
+	 //cout << "summ of numbers= " << suma << endl;
+
+
+	 //4
+
+
+	 //int times = 0;
+	 //
+	 //for (int i = 1; i <= 12; i++)
+	 //{
+	 //	times += i;
+	 //}
+	 //cout <<"clock rang "<< times <<" times" << endl;
+
+	 //5for without umovi-endless cycle
+
+
+
+	int number;
+	int summa = 0;
+	for (int i = 0;; i++)
+	{
+		cout << "Enter number:[0 to stop] " << endl; cin >> number;
+		summa += number;
+		if (number == 0) {
+			break;
+		}
+	}
+	cout << summa << endl;
+}
